0ab4585f436c640fd6696e4634cb48011a147524
Heera Bachada <h89611392@gmail.com>
2025-09-04 20:44:01 +0100

    Able to store more than one task in Tasks.json

--- Task_Tracker.py
+++ Task_Tracker.py
@@ -9,6 +9,7 @@ month = current_time.month
 year = current_time.year
 hour = current_time.hour
 minute = current_time.minute
+numTasks = 0
 if minute < 10:
     minute = f"0{minute}"
 time = f"{hour}:{minute}"
@@ -18,15 +19,19 @@ enter_tasks = True
 while enter_tasks == True:
     user_task = input("Please enter task")
     task_description = input("Please enter task description")
-    Python_task = loads(Task_file)
-    for task_details in Python_task:
-        Task_file = open("Tasks.json","w")
-        Tasks['Task name'] = user_task
-        Tasks['Task Description'] = task_description
-        Tasks['Date of task creation'] = taskDate
-
+    task_status = "todo"
+    numTasks += 1
+    task_description = input("Please enter task description")
+    tasks['name of task'] = user_task
+    tasks['task description'] = task_description
+    tasks['time task was created'] = taskDate
+    tasks['task id'] = "task" + str(numTasks)
+    tasks['status of task'] = task_status
+    print(tasks)
+    print(numTasks)
+    task_file = open("Tasks.json","a")
+    dump(tasks,task_file,indent=2)
     create_tasks = input("Do you want to enter more tasks in? (y/n)")
     if create_tasks == "n":
+        task_file.close()
         enter_tasks = False
-Task_file = open("Tasks.json","a")
-dump(Tasks,Task_file,indent=2)
